d0b9788cc84b4a0d5f6ab56cba65e618bcaeb23a
AdityaT248 <aditya.thakkar.2020@gmail.com>
2025-11-01 16:52:29 -0700

    Update to v0.3.14 - Slack Integration

--- floma.rb
+++ floma.rb
@@ -6,25 +6,25 @@
 class Floma < Formula
   desc "AI-Powered Workflow Automation Tool - No Code Required"
   homepage "https://getfloma.com"
-  version "0.3.13"
+  version "0.3.14"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.13/floma-macos-arm64"
-      sha256 "1adbf0a83f9f3275de57be917be461ae93970413cbf68d5663305b507f3bc31a"
+      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.14/floma-macos-arm64"
+      sha256 "3d958e08555465e145bbd9f5d9954dd70377a5fb91892111e575a0a0111d50b8"
     else
-      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.13/floma-macos-x86_64"
-      sha256 "8e9695c2fc8f241c6552213dd5b82cca7bdbe10f442d67d9117c60ff5091e7b9"
+      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.14/floma-macos-x86_64"
+      sha256 "a6d926e9ecb7c190caeda6ccec4f01dd2733f2b039f0a111d9d0ef6b8d7ea3ef"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.13/floma-linux-arm64"
-      sha256 "a5cc4ae95122bbea5982abcfc9f92824b99b540879a3248e7dd5ac0726d48898"
+      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.14/floma-linux-arm64"
+      sha256 "5a0884216a96d63800f78f1356038f029a3634aa9f2d18223a33140a47e7d12a"
     else
-      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.13/floma-linux-x86_64"
-      sha256 "fa6a2995d0a1800b37d66b4e2f2fdf8273139b3e051153b849035c0b40e3a9bc"
+      url "https://pub-3da12ce477ba4b969191f6514d7505ff.r2.dev/0.3.14/floma-linux-x86_64"
+      sha256 "2db91ca6f6b8e8434562443703a593404d0ff95a706f573b24033aa095077609"
     end
   end
 
